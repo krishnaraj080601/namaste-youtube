143b219056df50e61210d0769bb567a521a31917
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-10-07 20:43:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Body from './components/Body';
 import Head from './components/Head';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="text-9xl"> 
     <h1 className="text-3xl font-bold">Namaste react </h1>
     <Head/>
+    <Body/>
+    <Sidebar/>
     </div>
   );
 }
