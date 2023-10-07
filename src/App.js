d47ae3661231875daf90bcf2d6e3a5ea6805ed07
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-10-07 21:00:36 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Body from './components/Body';
 import Head from './components/Head';
-import Sidebar from './components/Sidebar';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
     <h1 className="text-3xl font-bold">Namaste react </h1>
     <Head/>
     <Body/>
-    <Sidebar/>
     </div>
   );
 }
