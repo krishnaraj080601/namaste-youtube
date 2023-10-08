4b5f54e1d403e2d1451c19ecd3b4311074f5ee20
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-10-08 10:33:56 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
+import { Provider } from 'react-redux';
 import './App.css';
 import Body from './components/Body';
 import Head from './components/Head';
+import store from './Utils/store';
 
 function App() {
   return (
+    <Provider store={store}>
     <div> 
     <Head/>
     <Body/>
     </div>
+    </Provider>
   );
 }
 
