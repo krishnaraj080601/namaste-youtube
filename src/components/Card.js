d88b48bc22e57db18251a1c78c73bfa1da6f6261
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-10-08 20:20:24 +0530

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,11 +8,14 @@ const Card = ({info}) => {
   return (
 
     <div>
-   <ul>
-   
-   <li>{title}</li>
-   <li>{channelTitle}</li>
-   </ul>
+    <div className="p-2 m-2 w-72 shadow-lg">
+    <img className="rounded-lg" alt="thumbnail" src={thumbnails.medium.url} />
+    <ul>
+      <li className="font-bold py-2">{title}</li>
+      <li>{channelTitle}</li>
+      <li>{statistics.viewCount} views</li>
+    </ul>
+  </div>
     
     </div>
   );
