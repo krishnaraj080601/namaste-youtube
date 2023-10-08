3149836d1a4061864dcde65c76c823038b6b839e
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-10-08 11:30:00 +0530

    commit

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -4,11 +4,14 @@ import Button from './Button'
 const ButtonList = () => {
   return (
     <div className="flex">
-    <Button/>
-    <Button/>
-    <Button/>
-    <Button/>
-    <Button/>
+    <Button name="All"/>
+    <Button name="gaming"/>
+    <Button name="live"/>
+    <Button name="Song"/>
+    <Button name="cricket"/>
+    <Button name="Football"/>
+    <Button name="News"/>
+    <Button name="Movies"/>
     </div>
   )
 }
