01f77671a1763241ec75259f84fd1f4e2378dea5
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-10-08 19:03:14 +0530

    commit

--- src/components/ViedoContainer.js
+++ src/components/ViedoContainer.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { YOUTUBE_VIEDOS_API } from '../Utils/constant';
 
 const ViedoContainer = () => {
+  const [Viedos,setViedos]=useState([]);
   useEffect(()=>{
     getViedos();
-  },[])
+  },[]);
   const getViedos=async()=>{
     const data=await fetch(YOUTUBE_VIEDOS_API);
     const json=await data.json();
-    console.log(json);
-  }
+    console.log(json.items);
+    setViedos(json.items)
+  };
   return (
-    <div>ViedoContainer</div>
+    <div>
+    </div>
   )
-}
+};
 
 export default ViedoContainer
---
+++ src/components/viedoCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const viedoCard = () => {
+  return (
+    <div>viedoCard</div>
+  )
+}
+
+export default viedoCard
