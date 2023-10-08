3b92fd46c46c4ca6f05142c663bc890ff9c78fa6
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-10-08 11:06:48 +0530

    commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Sidebar = () => {
+  const isMenuOpen=useSelector((store)=>store.app.isMenuopen);
+  if(!isMenuOpen) return null;
   return (
+    
     <div className="p-5 shadow-lg w-48">
     <ul>
     <li>Home </li>
