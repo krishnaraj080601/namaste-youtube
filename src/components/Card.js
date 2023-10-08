5d46e90f0927698b94c727fb496d673c89482f7a
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-10-08 20:17:21 +0530

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,19 +2,17 @@ import React from 'react'
 
 const Card = ({info}) => {
     console.log(info);
-    const { snippet, statistics } = info;
-  const { channelTitle, title, thumbnails } = snippet;
+   const { snippet, statistics} = info;
+   const { channelTitle, title, thumbnails } = snippet;
 
   return (
 
     <div>
+   <ul>
    
-    <ul>
-    <li>{thumbnails}</li>
-    <li>{channelTitle}</li>
-    <li>{statistics}</li>
-    <li>{title}</li>
-    </ul>
+   <li>{title}</li>
+   <li>{channelTitle}</li>
+   </ul>
     
     </div>
   );
