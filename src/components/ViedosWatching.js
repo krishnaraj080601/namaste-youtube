6ff3501952d9a6e49e8cbd0b3580643819d23f38
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-10-19 18:17:48 +0530

    commit

--- src/components/ViedosWatching.js
+++ src/components/ViedosWatching.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { closeMenu } from '../Utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
+import ViedosComments from './ViedosComments';
 
 const ViedosWatching = () => {
     const [searchParams]=useSearchParams();
@@ -11,10 +12,13 @@ const ViedosWatching = () => {
         dispatch(closeMenu())
 },[])
   return (
+    <>
     <div className="px-5">
     <iframe width="1200" height="600" src={"https://www.youtube.com/embed/"+ searchParams.get("v")}title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
   
     </div>
+    <ViedosComments/>
+    </>
 
     
   )
