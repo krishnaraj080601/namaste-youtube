5e2ebe931664be5aa7909e8179404131c08d5627
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-10-19 15:13:49 +0530

    commit

--- src/components/comments.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const comments = () => {
-  return (
-    <div>comments</div>
-  )
-}
-
-export default comments
---
+++ src/components/youtubecmt.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const youtubecmt = () => {
+  return (
+    <div>youtubecmt</div>
+  )
+}
+
+export default youtubecmt
