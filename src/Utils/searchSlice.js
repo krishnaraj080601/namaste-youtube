fa7706ff0adc228458f8b819965422d5e9970604
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-10-19 13:03:49 +0530

    commit

---
+++ src/Utils/searchSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const searchSlice=createSlice({
+    name:"search",
+    initialState:{},
+    reducers:{
+      cacheResults:(state,action)=>{
+        state={...action.payload,...state};
+      }
+    }
+});
+export default searchSlice;
