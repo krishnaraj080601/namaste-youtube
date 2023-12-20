d9a8960cbc2e86c7299e8c714c254c71bc28eeaf
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-20 10:22:03 +0530

    commit

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { toggleMenu } from '../Utils/appSlice';
 import { YOUTUBE_SEARCH_API } from '../Utils/constant';
 import { cacheResults } from '../Utils/searchSlice';
+import { Link } from 'react-router-dom';
 
 
 const Head = () => {
@@ -43,7 +44,7 @@ const Head = () => {
 
   return (
    
-    <div className="grid grid-flow-col p-2 m-2 shadow-lg ">
+    <div className="grid grid-flow-col p-1  shadow-lg bg-white w-full top-0  fixed z-50">
     <div className="flex col-span-3">
     <img onClick={()=>toggleMenuHandler()}
      className ="h-12 cursor-pointer"alt="menu"
@@ -60,12 +61,19 @@ const Head = () => {
     onFocus={() => setShowSuggestions(true)}
     onBlur={() => setShowSuggestions(false)}
     />
-    <button className="border border-gray-600 py-2 px-5 bg-gray-100 rounded-r-full">🔍</button>
+    <button onClick={() => window.location.href = "/results?search_query=" + searchQuery} className="border border-gray-400 rounded-r-full p-1 w-10 bg-gray-100">🔍</button>
     </div>
    {showSuggestions &&( <div className="fixed bg-white py-2 px-5 w-[37rem] shadow-lg rounded-lg border border-gray-100  ">
     <ul>
-    {suggestions.map(s=>(<li key={s} className='py-2 shadow-sm hover:bg-gray-100'> 🔍 {s}</li>))}
-    
+    {suggestions.map((s, index) => s[0] && (
+                                <li
+                                    onClick={() =>{}}
+                                    key={index}
+                                    className="py-2 px-3 shadow-sm hover:bg-gray-200"
+                                >
+                                    <Link to={`/results?search_query=${s}`} >🔍{s}</Link>
+                                </li>
+                            ))}
     </ul>
     </div>
    )}
