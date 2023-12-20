cc45227042d66e780f7cabf8cff0cf23d7df1610
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-20 23:39:57 +0530

    commit

--- src/components/Head.js
+++ src/components/Head.js
@@ -45,7 +45,7 @@ const Head = () => {
 
   return (
    
-    <div className="p-5 shadow-lg w-48">
+    <div className="pl-6 pr-2 py-1 grid grid-flow-col items-center shadow">
     <div className="flex col-span-1">
     <img onClick={()=>toggleMenuHandler()}
      className ="h-12 cursor-pointer"alt="menu"
